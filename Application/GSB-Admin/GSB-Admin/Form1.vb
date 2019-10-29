7da0f46432fa5a9c6a1d7aad4df14bb81ac26612
Tykok <56304246+Tykok@users.noreply.github.com>
2019-10-29 21:06:00 +0100

    Maquettes
    Les maquettes de PPE6
    Elles sont pas encores fini, il faut vérifier et il reste quelques trucs à changer en fonction de ce que vous préférez vous.

---
+++ Application/GSB-Admin/GSB-Admin/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class frm_principal
+
+
+    '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    'Bouton pour afficher et modifier le mot d passe d'un utilisateur
+    Private Sub btn_ChangeMdp_Click(sender As Object, e As EventArgs) Handles btn_ChangeMdp.Click
+
+        If btn_ChangeMdp.Text = ">" Then
+
+            btn_ChangeMdp.Text = "<"
+
+            grpB_reset.Visible = True
+            rb_Reset.Visible = True
+            rb_ResetNo.Visible = True
+            rb_resetYes.Visible = True
+
+            Me.Size = New Size(1200, Me.Size.Height)
+
+
+        Else
+
+            btn_ChangeMdp.Text = ">"
+
+            grpB_reset.Visible = False
+            rb_Reset.Visible = False
+            rb_ResetNo.Visible = False
+            rb_resetYes.Visible = False
+
+            Me.Size = New Size(793, Me.Size.Height)
+        End If
+
+
+    End Sub
+
+    '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    'Bouton d'ouverture du formulaire de création d'un nouvel utilisateur
+    Private Sub btn_CreateUser_Click(sender As Object, e As EventArgs) Handles btn_CreateUser.Click
+        Me.Close()
+        creatUser.Show()
+    End Sub
+End Class
---
+++ Application/GSB-Admin/GSB-Admin/connexion.vb
@@ -0,0 +1,20 @@
+﻿Public Class connexion
+
+
+    '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    'Boutton pour quitter l'application
+    Private Sub btn_Quitte_Click(sender As Object, e As EventArgs) Handles btn_Quitte.Click
+        Me.Close()
+    End Sub
+
+
+    '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    'Bouton de connexion
+    Private Sub btn_Submit_Click(sender As Object, e As EventArgs) Handles btn_Submit.Click
+        If txtB_Login.Text = "root" & txtB_MDP.Text = "root" Then
+            frm_principal.Show()
+        Else
+            lbl_Erreur.Text = "NON"
+        End If
+    End Sub
+End Class
